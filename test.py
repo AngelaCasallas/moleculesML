18233deea13d6de00f27376201fdd57d6bf28eb5
AngelaC <camilacasallas11@gmail.com>
2023-03-11 13:08:14 -0500

    try lists

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 def greetings(x): #input
     print("Hello " + x)
 
-greetings("Coco")
 
-def sum(x,y):
-    print(x + y)
 
-sum(1,2)
+conures = ("Coco","Cookie","Popito") #list
+
+message = "Hello"
+for c in conures:
+    #greetings(c)
+    message = message + " " + c
+print (message)
+
+numbers = [1,2,3,4,5,6,3,44,55,67,34]
+#create a functions to print n numbers
+#for n in numbers:
+    #print (n)
+
+#Create a function called sumAll(n) to sum n numbers
+s = 0
+for n in numbers:
+    s = s + n
+print (s)
