570d5d7c6adf964229ebc1c4f6afe7c1effd28fd
AngelaC <camilacasallas11@gmail.com>
2023-03-21 10:56:47 -0400

    basic pygame

---
+++ learning.py
@@ -0,0 +1,27 @@
+#dna_letters = ["A", "G", "T", "C", "U", "A", "G"]
+dna_letters = "AGTCUAGUUUU"
+to_find = "U"
+for rna in dna_letters:
+    
+    if rna == to_find:
+        print("Found! " + rna)
+
+numbers = [1,5,8,9,20,30,5,5,60,5]
+min = 5
+max = 10
+for five in numbers:
+    if five >= min and five <= max:
+        print("Found " + str(five))
+
+set = {"a", "e", "i", "o", "u"}
+for n in set:
+ print (n)
+
+
+numbers = {1,2,3,4,5,4,5,55,5}
+#sum = sum (numbers)
+#print(sum(numbers))
+n = 1
+for n 
+
+
