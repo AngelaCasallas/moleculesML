a34da170d1b62aab2c62edfe5741f784fc1b2f15
AngelaC <camilacasallas11@gmail.com>
2023-03-29 11:59:43 -0400

    My First DNA Problem

---
+++ book.py
@@ -0,0 +1,13 @@
+#CALCULATING AT CONTENT
+
+#Write a program that will print out the AT content of this DNA sequence. Hint: you can use normal mathematical symbols like add (+), 
+#subtract (-), multiply (*), divide (/) and parentheses to carry out calculations on numbers in Python. 
+
+dna_sequence = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+dna_seq_lenght = len(dna_sequence) #lenght of the sequence
+countT = dna_sequence.count("T") #counts how many T's present in sequence
+countA = dna_sequence.count("A") #counts how many A's present in sequence
+sum = countT + countA
+division = sum / float(dna_seq_lenght) #divides the number of A's and T's by the total amount of nucleotides in sequence
+multiply = division * 100
+print("The AT content in the DNA sequence is " + str(multiply) + "%")
