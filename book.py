699f59f07893bd5fd8a5247643b0deb9057679a2
AngelaC <camilacasallas11@gmail.com>
2023-04-01 11:40:08 -0400

    read dna seq

--- book.py
+++ book.py
@@ -3,7 +3,14 @@
 #Write a program that will print out the AT content of this DNA sequence. Hint: you can use normal mathematical symbols like add (+), 
 #subtract (-), multiply (*), divide (/) and parentheses to carry out calculations on numbers in Python. 
 
-dna_sequence = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+
+#gett DNA from file dna.txt
+dna_file = open("dna.txt", "r") #open file and dna file is now a file object from file class
+dna_sequence = dna_file.read() #using read method from File class to read content of the file
+dna_file.close()
+
+
+#dna_sequence = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
 dna_seq_lenght = len(dna_sequence) #lenght of the sequence
 countT = dna_sequence.count("T") #counts how many T's present in sequence
 countA = dna_sequence.count("A") #counts how many A's present in sequence
@@ -11,3 +18,5 @@ sum = countT + countA
 division = sum / float(dna_seq_lenght) #divides the number of A's and T's by the total amount of nucleotides in sequence
 multiply = division * 100
 print("The AT content in the DNA sequence is " + str(multiply) + "%")
+
+print(countA)
